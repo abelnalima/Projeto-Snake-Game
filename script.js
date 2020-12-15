c508800a67e3f9cef542fe22b9d9d7916efad2dd
abelnalima <abel.na.lima@gmail.com>
2020-12-15 20:23:09 -0300

    capturar teclado, atravessar parede

--- script.js
+++ script.js
@@ -8,31 +8,46 @@ snake[0] = {
 }
 let direction = "right";
 
-function criarBG() {
+function criarBG() { //Cria a "area de jogo"
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarSnake() {
+function criarSnake() { //Cria o marcador da snake
     for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function iniciarJogo() {
+document.addEventListener('keydown', update); //Escuta as teclas do teclado
+
+function update(event) { //Atualiza a direção de acordo com o jogador
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo() { //Define o movimento da snake
+    //Alteração das propriedades para permitir que a snake "atravesse" a tela
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
     criarBG();
     criarSnake();
 
-    let snakeX = snake[0].x;
+    let snakeX = snake[0].x; //Coordenadas iniciais da snake
     let snakeY = snake[0].y;
 
-    if (direction == "right") snakeX += box;
-    if (direction == "left") snakeX -= box;
+    if (direction == "right") snakeX += box; //Adiciona uma box na
+    if (direction == "left") snakeX -= box; //direção do movimento
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    snake.pop(); //remove uma box mantendo o comprimento da snake
 
     let newHead = {
         x: snakeX,
